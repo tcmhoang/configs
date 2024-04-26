49c28435248ed40b8654f8c3e050cb0dd6d3bb10
Conrad Hoang <conrad@sprqpt.com>
2024-04-26 11:50:35 -0400

    direnv config
    Signed-off-by: Conrad Hoang <conrad@sprqpt.com>

--- Configs/shell/.config/fish/config.fish
+++ Configs/shell/.config/fish/config.fish
@@ -1,4 +1,3 @@
-
 # =============================================================================
 #
 # Utility functions for zoxide.
@@ -118,114 +117,120 @@ alias z=__zoxide_z
 abbr --erase zi &>/dev/null
 alias zi=__zoxide_zi
 
-abbr -a cd 'z'
-abbr -a ci 'zi'
+abbr -a cd z
+abbr -a ci zi
 
 
 fish_add_path /home/tcmhoang/.spicetify
 zoxide init fish | source
-starship  init fish | source
+starship init fish | source
+direnv hook fish | source
+
+set -g direnv_fish_mode eval_on_arrow # trigger direnv at prompt, and on every arrow-based directory change (default)
+set -g direnv_fish_mode eval_after_arrow # trigger direnv at prompt, and only after arrow-based directory changes before executing command
+set -g direnv_fish_mode disable_arrow # trigger direnv at prompt only, this is similar functionality to the original behavior
+
 
 if status --is-interactive
-	if ! set -q TMUX
-		exec tmux
-	end
+    if ! set -q TMUX
+        exec tmux
+    end
 end
 
-if command -v bat > /dev/null
-	abbr -a cat 'bat'
+if command -v bat >/dev/null
+    abbr -a cat bat
 end
 
-if command -v eza > /dev/null
-	abbr -a l 'eza --icons auto'
-	abbr -a ls 'eza --icons auto'
-	abbr -a ll 'eza -l --icons auto'
-	abbr -a lll 'eza -la --icons auto'
-	abbr -a llg 'eza -la --git --icons auto'
+if command -v eza >/dev/null
+    abbr -a l 'eza --icons auto'
+    abbr -a ls 'eza --icons auto'
+    abbr -a ll 'eza -l --icons auto'
+    abbr -a lll 'eza -la --icons auto'
+    abbr -a llg 'eza -la --git --icons auto'
 else
-	abbr -a l 'ls'
-	abbr -a ll 'ls -l'
-	abbr -a lll 'ls -la'
+    abbr -a l ls
+    abbr -a ll 'ls -l'
+    abbr -a lll 'ls -la'
 end
 
 
 function ssh
-	switch $argv[1]
-	case "*.amazonaws.com"
-		env TERM=xterm /usr/bin/ssh $argv
-	case "ubuntu@"
-		env TERM=xterm /usr/bin/ssh $argv
-	case "*"
-		/usr/bin/ssh $argv
-	end
+    switch $argv[1]
+        case "*.amazonaws.com"
+            env TERM=xterm /usr/bin/ssh $argv
+        case "ubuntu@"
+            env TERM=xterm /usr/bin/ssh $argv
+        case "*"
+            /usr/bin/ssh $argv
+    end
 end
 
 function apass
-	if test (count $argv) -ne 1
-		pass $argv
-		return
-	end
+    if test (count $argv) -ne 1
+        pass $argv
+        return
+    end
 
-	asend (pass $argv[1] | head -n1)
+    asend (pass $argv[1] | head -n1)
 end
 
 function qrpass
-	if test (count $argv) -ne 1
-		pass $argv
-		return
-	end
+    if test (count $argv) -ne 1
+        pass $argv
+        return
+    end
 
-	qrsend (pass $argv[1] | head -n1)
+    qrsend (pass $argv[1] | head -n1)
 end
 
 function asend
-	if test (count $argv) -ne 1
-		echo "No argument given"
-		return
-	end
+    if test (count $argv) -ne 1
+        echo "No argument given"
+        return
+    end
 
-	adb shell input text (echo $argv[1] | sed -e 's/ /%s/g' -e 's/\([#[()<>{}$|;&*\\~"\'`]\)/\\\\\1/g')
+    adb shell input text (echo $argv[1] | sed -e 's/ /%s/g' -e 's/\([#[()<>{}$|;&*\\~"\'`]\)/\\\\\1/g')
 end
 
 function qrsend
-	if test (count $argv) -ne 1
-		echo "No argument given"
-		return
-	end
+    if test (count $argv) -ne 1
+        echo "No argument given"
+        return
+    end
 
-	qrencode -o - $argv[1] | feh --geometry 500x500 --auto-zoom -
+    qrencode -o - $argv[1] | feh --geometry 500x500 --auto-zoom -
 end
 
 function limit
-	numactl -C 0,1,2 $argv
+    numactl -C 0,1,2 $argv
 end
 
 
 # Type - to move up to top parent dir which is a repository
 function d
-	while test $PWD != "/"
-		if test -d .git
-			break
-		end
-		cd ..
-	end
+    while test $PWD != /
+        if test -d .git
+            break
+        end
+        cd ..
+    end
 end
 
 # Fish git prompt
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showuntrackedfiles yes
+set __fish_git_prompt_showdirtystate yes
 set __fish_git_prompt_showstashstate ''
-set __fish_git_prompt_showupstream 'none'
+set __fish_git_prompt_showupstream none
 set -g fish_prompt_pwd_dir_length 3
 
 # colored man output
 # from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
-setenv LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
-setenv LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
-setenv LESS_TERMCAP_me \e'[0m'           # end mode
-setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
-setenv LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
-setenv LESS_TERMCAP_ue \e'[0m'           # end underline
+setenv LESS_TERMCAP_mb \e'[01;31m' # begin blinking
+setenv LESS_TERMCAP_md \e'[01;38;5;74m' # begin bold
+setenv LESS_TERMCAP_me \e'[0m' # end mode
+setenv LESS_TERMCAP_se \e'[0m' # end standout-mode
+setenv LESS_TERMCAP_so \e'[38;5;246m' # begin standout-mode - info box
+setenv LESS_TERMCAP_ue \e'[0m' # end underline
 setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
 
 setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
@@ -235,7 +240,7 @@ setenv FZF_DEFAULT_OPTS "--height 20% \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-setenv EDITOR 'nvim'
+setenv EDITOR nvim
 
 
 setenv OS_USERNAME jfrg@csail.mit.edu
@@ -244,36 +249,36 @@ setenv OS_AUTH_URL https://nimbus.csail.mit.edu:5001/v2.0
 setenv OS_IMAGE_API_VERSION 1
 setenv OS_VOLUME_API_VERSION 2
 function penv -d "Set up environment for the PDOS openstack service"
-	env OS_PASSWORD=(pass www/mit-openstack | head -n1) OS_TENANT_NAME=pdos OS_PROJECT_NAME=pdos $argv
+    env OS_PASSWORD=(pass www/mit-openstack | head -n1) OS_TENANT_NAME=pdos OS_PROJECT_NAME=pdos $argv
 end
 
 function fish_user_key_bindings
-	bind \cz 'fg>/dev/null ^/dev/null'
-	if functions -q fzf_key_bindings
-		fzf_key_bindings
-	end
+    bind \cz 'fg>/dev/null ^/dev/null'
+    if functions -q fzf_key_bindings
+        fzf_key_bindings
+    end
 end
 
 
 function fish_greeting
-	echo
-	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
-	echo -e (puptime  | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
-	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
-	echo -e " \\e[1mDisk usage:\\e[0m"
-	echo
-	echo -ne (\
+    echo
+    echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+    echo -e (puptime  | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+    echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+    echo -e " \\e[1mDisk usage:\\e[0m"
+    echo
+    echo -ne (\
 		df -l -h | grep -E 'dev/(xvda|sd|mapper|nvme)' | \
 		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
 		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
 		paste -sd ' '\
 	)
-	echo
+    echo
 
-	echo -e " \\e[1mNetwork:\\e[0m"
-	echo
-	# http://tdt.rocks/linux_network_interface_naming.html
-	echo -ne (\
+    echo -e " \\e[1mNetwork:\\e[0m"
+    echo
+    # http://tdt.rocks/linux_network_interface_naming.html
+    echo -ne (\
 		ip addr show up scope global | \
 			grep -E ': <|inet' | \
 			sed \
@@ -299,46 +304,46 @@ function fish_greeting
 			sed 's/$/\\\e[0m/' | \
 			sed 's/^/\t/' \
 		)
-	echo
+    echo
 
-	set r (random 0 100)
-	if [ $r -lt 5 ] # only occasionally show backlog (5%)
-		echo -e " \e[1mBacklog\e[0;32m"
-		set_color blue
-		echo "  [project] <description>"
-		echo
-	end
+    set r (random 0 100)
+    if [ $r -lt 5 ] # only occasionally show backlog (5%)
+        echo -e " \e[1mBacklog\e[0;32m"
+        set_color blue
+        echo "  [project] <description>"
+        echo
+    end
 
-	set_color normal
-	echo -e " \e[1mTODOs\e[0;32m"
-	echo
-	if [ $r -lt 10 ]
-		# unimportant, so show rarely
-		set_color cyan
-		# echo "  [project] <description>"
-	end
-	if [ $r -lt 25 ]
-		# back-of-my-mind, so show occasionally
-		set_color green
-		# echo "  [project] <description>"
-	end
-	if [ $r -lt 50 ]
-		# upcoming, so prompt regularly
-		set_color yellow
-		# echo "  [project] <description>"
-	end
+    set_color normal
+    echo -e " \e[1mTODOs\e[0;32m"
+    echo
+    if [ $r -lt 10 ]
+        # unimportant, so show rarely
+        set_color cyan
+        # echo "  [project] <description>"
+    end
+    if [ $r -lt 25 ]
+        # back-of-my-mind, so show occasionally
+        set_color green
+        # echo "  [project] <description>"
+    end
+    if [ $r -lt 50 ]
+        # upcoming, so prompt regularly
+        set_color yellow
+        # echo "  [project] <description>"
+    end
 
-	# urgent, so prompt always
-	set_color red
-	# echo "  [project] <description>"
+    # urgent, so prompt always
+    set_color red
+    # echo "  [project] <description>"
 
-	echo
+    echo
 
-	if test -s ~/todo
-		set_color magenta
-		cat todo | sed 's/^/ /'
-		echo
-	end
+    if test -s ~/todo
+        set_color magenta
+        cat todo | sed 's/^/ /'
+        echo
+    end
 
-	set_color normal
+    set_color normal
 end
