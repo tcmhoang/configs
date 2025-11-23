8f522aa45736ae072e29615097077b39b52890c4
Conrad Hoang <conrad@sprqpt.com>
2025-11-23 08:36:41 -0500

    Reconfig leap plugin

--- Configs/editor/.config/nvim/init.lua
+++ Configs/editor/.config/nvim/init.lua
@@ -373,7 +373,21 @@ require("lazy").setup({
 	{
 		'ggandor/leap.nvim',
 		config = function()
-			require('leap').create_default_mappings()
+			vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
+			vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
+			leap = require('leap')
+
+			leap.opts.preview = function (ch0, ch1, ch2)
+			  return not (
+			    ch1:match('%s')
+			    or (ch0:match('%a') and ch1:match('%a') and ch2:match('%a'))
+			  )
+			end
+			leap.opts.equivalence_classes = {
+			  ' \t\r\n', '([{', ')]}', '\'"`'
+			}
+			require('leap.user').set_repeat_keys('<enter>', '<backspace>')
+
 		end
 	},
 	-- better %
